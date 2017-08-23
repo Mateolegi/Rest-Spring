3f15cfcf2ef69abd2a4b76635a42adc31348f7e0
Mateo Leal <mateolegi@gmail.com>
2017-08-22 22:39:25 -0500

    My first API Restful af

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Initializes the Spring application
+ * Created by mateo on 22/08/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
